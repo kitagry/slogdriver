acbfb971b036e97e5610d0525e24b6127cea9d59
Ryo Kitagawa <kitadrum50@gmail.com>
2023-09-18 18:05:04 +0900

    feat: support opentelemetry

--- trace.go
+++ trace.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"log/slog"
 
-	"go.opencensus.io/trace"
+	opencensusTrace "go.opencensus.io/trace"
+	opentelemetryTrace "go.opentelemetry.io/otel/trace"
 )
 
 func (c *cloudLoggingHandler) handleTrace(ctx context.Context, r *slog.Record) {
@@ -17,7 +18,12 @@ func (c *cloudLoggingHandler) handleTrace(ctx context.Context, r *slog.Record) {
 		return
 	}
 
-	span := trace.FromContext(ctx)
+	c.handleOpencensusTrace(ctx, r)
+	c.handleOpentelemetryTrace(ctx, r)
+}
+
+func (c *cloudLoggingHandler) handleOpencensusTrace(ctx context.Context, r *slog.Record) {
+	span := opencensusTrace.FromContext(ctx)
 	if span == nil {
 		return
 	}
@@ -26,6 +32,19 @@ func (c *cloudLoggingHandler) handleTrace(ctx context.Context, r *slog.Record) {
 	r.AddAttrs(
 		slog.String(TraceKey, fmt.Sprintf("projects/%s/traces/%s", c.opts.ProjectID, spanCtx.TraceID.String())),
 		slog.String(SpanIDKey, spanCtx.SpanID.String()),
+		slog.Bool(TraceSampledKey, spanCtx.IsSampled()),
+	)
+}
+
+func (c *cloudLoggingHandler) handleOpentelemetryTrace(ctx context.Context, r *slog.Record) {
+	spanCtx := opentelemetryTrace.SpanContextFromContext(ctx)
+
+	if !spanCtx.HasTraceID() || !spanCtx.HasSpanID() {
+		return
+	}
+	r.AddAttrs(
+		slog.String(TraceKey, fmt.Sprintf("projects/%s/traces/%s", c.opts.ProjectID, spanCtx.TraceID().String())),
+		slog.String(SpanIDKey, spanCtx.SpanID().String()),
 		slog.Bool(TraceSampledKey, spanCtx.IsSampled()),
 	)
 }
--- trace_test.go
+++ trace_test.go
@@ -7,22 +7,61 @@ import (
 	"testing"
 
 	"github.com/kitagry/slogdriver"
-	"go.opencensus.io/trace"
+	opencensusTrace "go.opencensus.io/trace"
+	"go.opentelemetry.io/contrib/detectors/gcp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
-func TestCloudLoggingHandler_HandleTraceShouldHaveTraceKeys(t *testing.T) {
+func TestCloudLoggingHandler_HandleTraceShouldHaveOpencensusTraceKeys(t *testing.T) {
 	t.Setenv("GOOGLE_CLOUD_PROJECT", "test-project")
 
 	var buf bytes.Buffer
 	logger := slogdriver.New(&buf, slogdriver.HandlerOptions{})
 
-	ctx, span := trace.StartSpan(context.Background(), "test-span")
+	ctx, span := opencensusTrace.StartSpan(context.Background(), "test-span")
 	defer span.End()
 
 	logger.InfoContext(ctx, "Hello World")
 
 	var got map[string]any
 	err := json.NewDecoder(&buf).Decode(&got)
+	if err != nil {
+		t.Fatalf("failed to decode json: %+v", err)
+	}
+
+	_, ok := got[slogdriver.TraceKey]
+	if !ok {
+		t.Errorf("log should have key=%s, got %v", slogdriver.TraceKey, got)
+	}
+}
+
+func TestCloudLoggingHandler_HandleTraceShouldHaveOpentelemetryTraceKeys(t *testing.T) {
+	t.Setenv("GOOGLE_CLOUD_PROJECT", "test-project")
+
+	var buf bytes.Buffer
+	logger := slogdriver.New(&buf, slogdriver.HandlerOptions{})
+
+	ctx := context.Background()
+
+	res, err := resource.New(ctx, resource.WithDetectors(gcp.NewDetector()), resource.WithTelemetrySDK())
+	if err != nil {
+		t.Fatalf("failed to create resource: %+v", err)
+	}
+
+	tp := sdktrace.NewTracerProvider(sdktrace.WithResource(res))
+	defer tp.Shutdown(ctx)
+	otel.SetTracerProvider(tp)
+
+	tracer := otel.Tracer("test-tracer")
+	ctx, span := tracer.Start(context.Background(), "test-span")
+	defer span.End()
+
+	logger.InfoContext(ctx, "Hello World")
+
+	var got map[string]any
+	err = json.NewDecoder(&buf).Decode(&got)
 	if err != nil {
 		t.Fatalf("failed to decode json: %+v", err)
 	}
